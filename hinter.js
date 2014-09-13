9a8b87469cc44999e38694358bc7b84f12ee6b0f
Belleve Invis <belleve@typeof.net>
2014-09-13 21:31:14 +0800

    Added LOW_PPEM_LIMIT and MIN_LOW_PPEM_STEM_WIDTH

--- hinter.js
+++ hinter.js
@@ -22,6 +22,10 @@ function hint(glyph, ppem, strategy) {
 	var COLLISION_MIN_OVERLAP_RATIO = strategy.COLLISION_MIN_OVERLAP_RATIO || 0.2;
 
 	var MIN_TOUCHED_STEM_WIDTH = strategy.MIN_TOUCHED_STEM_WIDTH || 1;
+	var LOW_PPEM_LIMIT = strategy.LOW_PPEM_LIMIT || 13;
+	var MIN_LOW_PPEM_STEM_WIDTH = strategy.MIN_LOW_PPEM_STEM_WIDTH || 1;
+	
+	var mtsw = ppem <= LOW_PPEM_LIMIT ? MIN_LOW_PPEM_STEM_WIDTH : MIN_TOUCHED_STEM_WIDTH;
 
 	var ABLATION_IN_RADICAL = strategy.ABLATION_IN_RADICAL || 1;
 	var ABLATION_RADICAL_EDGE = strategy.ABLATION_RADICAL_EDGE || 2;
@@ -86,8 +90,8 @@ function hint(glyph, ppem, strategy) {
 	function calculateWidth(w, mstw){
 		mstw = mstw || 1;
 		if(w < mstw * uppx) return mstw * uppx;
-		else if (w < (1 + mstw) * uppx) return uppx * Math.round(WIDTH_FACTOR_X 
-			* (w / uppx / WIDTH_FACTOR_X + clamp((ppem - MIN_ADJUST_PPEM) / (MAX_ADJUST_PPEM - MIN_ADJUST_PPEM)) * (1 - w / uppx / WIDTH_FACTOR_X)));
+//		else if (w < (1 + mstw) * uppx) return uppx * Math.round(WIDTH_FACTOR_X 
+//			* (w / uppx / WIDTH_FACTOR_X + clamp((ppem - MIN_ADJUST_PPEM) / (MAX_ADJUST_PPEM - MIN_ADJUST_PPEM)) * (1 - w / uppx / WIDTH_FACTOR_X)));
 		else return Math.round(w / uppx) * uppx;
 	}
 
@@ -396,7 +400,7 @@ function hint(glyph, ppem, strategy) {
 		for(var j = 0; j < stems.length; j++) {
 			debugger;
 			var sb = spaceBelow(stems, overlaps, j, pixelBottom - uppx);
-			var wr = calculateWidth(stems[j].width, MIN_TOUCHED_STEM_WIDTH);
+			var wr = calculateWidth(stems[j].width, mtsw);
 			var w = Math.min(round(wr), round(stems[j].touchwidth + sb - uppx));
 			if(w < uppx + 1) continue;
 			if(sb + stems[j].touchwidth > wr + uppx - 1 && stems[j].ytouch - wr >= pixelBottom + uppx - 1 || atGlyphBottom(stems[j]) && stems[j].ytouch - wr >= pixelBottom - 1) {
@@ -410,7 +414,7 @@ function hint(glyph, ppem, strategy) {
 		for(var j = stems.length - 1; j >= 0; j--) if(!allocated[j]){
 			var sb = spaceBelow(stems, overlaps, j, pixelBottom - uppx);
 			var sa = spaceAbove(stems, overlaps, j, pixelTop);
-			var wr = calculateWidth(stems[j].width, MIN_TOUCHED_STEM_WIDTH);
+			var wr = calculateWidth(stems[j].width, mtsw);
 			var w = Math.min(round(wr), round(stems[j].touchwidth + sb + sa - 2 * uppx));
 			if(w < uppx + 1) continue;
 			if(sa > 1.75 * uppx && stems[j].ytouch < avaliables[j].high * uppx) {
@@ -457,12 +461,12 @@ function hint(glyph, ppem, strategy) {
 					stem.low[k][p].ytouch = stem.ytouch - w;
 					stem.low[k][p].touched = true;
 					if(k === 0) {
-						if(stem.touchwidth >= round(stem.width) && Math.abs(stem.ytouch - stem.touchwidth - pixelBottom) < 1 && stem.width >= uppx) {
+						if(stem.touchwidth >= round(stem.width) && Math.abs(stem.ytouch - stem.touchwidth - pixelBottom) < 1 && stem.width >= mtsw * uppx) {
 							stem.touchwidth = stem.width;
 							stem.low[k][p].keypoint = true;
 							topkey = ['ROUND', stem.low[0][0], stem.low[0][0].yori, pixelBottom]
 							bottomkey = ['ALIGNW', stem.low[0][0], stem.high[0][0]]
-						} else if(stem.touchwidth >= round(stem.width) && stem.ytouch - stem.width >= pixelBottom && stem.width >= uppx) {
+						} else if(stem.touchwidth >= round(stem.width) && stem.ytouch - stem.width >= pixelBottom && stem.width >= mtsw * uppx) {
 							stem.touchwidth = stem.width;
 							bottomkey = ['ALIGNW', stem.high[0][0], stem.low[0][0]]
 						}
